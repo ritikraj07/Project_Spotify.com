6038ce3f9acf856258ef601f6f23c73cb5f61504
ritikraj07 <ritikra3rrr@gmail.com>
2023-01-04 15:01:47 +0530

    file set

---
+++ UTILITY/api_call.js
@@ -0,0 +1,105 @@
+import getKeys from "./keys.js";
+
+const keys = getKeys();
+
+async function getToken() {
+  const clientId = keys.CLIENT_ID;
+  const clientSecret = keys.CLIENT_SECRET;
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  return data.access_token;
+}
+
+async function refreshToken() {
+  let newToken = await getToken();
+  localStorage.setItem("spotify_token", newToken);
+}
+
+async function getPlaylists(category, limit, TOKEN) {
+  let response = await fetch(
+    `https://api.spotify.com/v1/browse/categories/${category}/playlists?country=IN&offset=0&limit=${limit}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  let data = await response.json();
+  return data.playlists.items;
+}
+
+async function getTrack(playlistID, TOKEN) {
+  let res = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistID}/tracks?offset=0&limit=20`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  let data = await res.json();
+  return data;
+}
+
+async function getAlbumTrack(albumID, TOKEN) {
+  let res = await fetch(
+    `https://api.spotify.com/v1/albums/${albumID}/tracks?offset=0&limit=20`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  let data = await res.json();
+  return data;
+}
+
+async function getAllSearchResults(query, type, limit, TOKEN) {
+  let response = await fetch(
+    `https://api.spotify.com/v1/search?type=${type.join()}&q=${query}?offset=0&limit=${limit}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  let data = await response.json();
+  return data;
+}
+
+async function getCategoryPlaylists(category, offset, limit, TOKEN) {
+  let response = await fetch(
+    `https://api.spotify.com/v1/browse/categories/${category}/playlists?country=IN&offset=${offset}&limit=${limit}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  let data = await response.json();
+  return data.playlists.items;
+}
+
+export {
+  refreshToken,
+  getPlaylists,
+  getTrack,
+  getAllSearchResults,
+  getAlbumTrack,
+  getCategoryPlaylists,
+};
+
---
+++ UTILITY/keys.js
@@ -0,0 +1,9 @@
+const getKeys = () => {
+    return {
+      CLIENT_ID: "5fae4890a7f244b29c841946eaba7a8d",
+      CLIENT_SECRET: "49afa59c62c645809a9f82a37fba5eca",
+    };
+  };
+      
+  export default getKeys;
+  
