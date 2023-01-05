f01a913182bd85703c807f5bbcf763cc4e545364
priyadarshanagrawal <priyadarshanagrawal052@gmail.com>
2023-01-06 02:40:01 +0530

    signup_login

---
+++ JS/login.js
@@ -0,0 +1,49 @@
+let users=JSON.parse(localStorage.getItem("spotify_users"))||[];
+let flag=false;
+let verify=()=>{
+    let flag=false;
+    let email_mobile_number=document.getElementById("email_mobile_number").value;
+    let password=document.getElementById("password").value;
+    let validuser = false;
+    let usernotallowed = false;
+
+    users.map(function (el) {
+        if (el.email == email_mobile_number && el.password == password) {
+            validuser = true;
+            
+        } 
+        else if (el.email == email_mobile_number) {
+            usernotallowed = true;
+            return;
+        }
+    });
+    if (validuser) {
+        document.getElementById("loading").style.display = "block";
+        setTimeout(function(){
+            location.href = "../HTML/index.html";
+        },3000)
+        
+        // location.href = "index.html";
+    } else if (usernotallowed) {
+        alert("Incorrect email or password.");
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+    } else {
+        alert("Please Register Your Account");
+        location.href = "../HTML/login.html";
+    }
+}
+
+let tosignup=()=>{
+    window.location.href="../HTML/signup.html"
+}
+document.getElementById("signup-btn").addEventListener("click",tosignup);
+
+
+function on() {
+    document.getElementById("login_button").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("login_button").style.display = "none";
+  }
